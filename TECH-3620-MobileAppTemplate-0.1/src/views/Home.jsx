ce900887e24bc07a6018ff2f26cfaefcfa0ecec4
Robert Skwiat <rskwiat@gmail.com>
2023-10-16 21:14:07 -0400

    October 16th work, setting up home page search bar

--- TECH-3620-MobileAppTemplate-0.1/src/views/Home.jsx
+++ TECH-3620-MobileAppTemplate-0.1/src/views/Home.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, SafeAreaView, StyleSheet, Platform } from "react-native";
-import { Text, Button } from "@rneui/base";
+import { Text, Button, SearchBar } from "@rneui/base";
 
 import AppHeader from "../components/Header";
 
 const HomeView = ({ navigation }) => {
+	const [search, setSearch] = useState("");
+
 	return (
 		<SafeAreaView>
 			<AppHeader navigation={navigation} />
 			<View style={styles.container}>
-				<Text>Home</Text>
+				<SearchBar
+					placeholder="Please search for your reciepe..." 
+					lightTheme
+					onChangeText={(value) => setSearch(value)}
+					value={search}
+					containerStyle={styles.inputContainer}
+					inputContainerStyle={styles.input}
+				/>
+
+				<Text style={styles.header}>Today's Recommended:</Text>
+				
 				<Button title="Favorites" onPress={() => navigation.navigate("Favorites")} />
 				<Button title="Recipes" onPress={() => navigation.navigate("Recipe")} />
 			</View>
@@ -20,6 +32,18 @@ const HomeView = ({ navigation }) => {
 const styles = StyleSheet.create({
 	container: {
 		marginHorizontal: Platform.OS === "ios" ? 18 : 20,
+	},
+	inputContainer: {
+		padding: 0,
+		marginVertical: 10,		
+	},
+	input: {
+		backgroundColor: "#fff",
+	},
+	header: {
+		fontStyle: "italic",
+		fontSize: 15,
+		marginBottom: 10,
 	}
 })
 
